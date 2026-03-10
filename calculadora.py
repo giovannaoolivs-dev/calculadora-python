fdcdd913b0d0db7ae496c5f1925d7df43901e3fb
giovannaoolivs-dev <giovannaoolivs@gmail.com>
2026-03-09 22:27:55 -0300

    Atualizar o calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,33 @@
+print("=== Calculadora em Python ===")
 
+while True:
+    print("\nEscolha uma operação:")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    print("5 - Sair")
+
+    opcao = input("Digite a opção desejada: ")
+
+    if opcao == "5":
+        print("Encerrando a calculadora...")
+        break
+
+    if opcao in ["1", "2", "3", "4"]:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+
+        if opcao == "1":
+            resultado = num1 + num2
+        elif opcao == "2":
+            resultado = num1 - num2
+        elif opcao == "3":
+            resultado = num1 * num2
+        elif opcao == "4":
+            resultado = num1 / num2
+
+        print("Resultado:", resultado)
+
+    else:
+        print("Opção inválida!")
